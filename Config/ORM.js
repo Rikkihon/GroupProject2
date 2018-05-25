e41f3732865a756fcd3d851301058e0766b12916
Rikki Honnold <rikki@er-10-129-72-207.wireless.umn.edu>
2018-05-24 21:18:16 -0500

    removed a file

--- Config/ORM.js
+++
@@ -1,4 +0,0 @@
-// Here is the O.R.M. where you write functions that takes inputs and conditions
-// and turns them into database commands like SQL.
-//this is from the solved homework assignment from week 14
-
--- Config/applicant.js
+++
@@ -1,9 +0,0 @@
-//not sure where to use this or but I think it might be a component that we will need. Unsure.
-module.exports = function(recommender)
-{
-    var recommender = sequelize.define("recommender", {
-        comment: Datatypes.STRING,
-        id: Datatypes.INTEGER
-    })
-    return recommender
-}
