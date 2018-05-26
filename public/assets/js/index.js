a15dea6ebd87373b952e1ac37025b400cab28365
Joan Marie Verba <verba001@umn.edu>
2018-05-26 13:49:41 -0500

    changes

---
+++ public/assets/js/index.js
@@ -0,0 +1,62 @@
+
+ $(".sidebutton").on("click", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+    let inputName = $("#name").val().trim();
+    let inputRequest = $("#request").val().trim();
+
+        let newRequest = {
+            user: inputName,
+            post: inputRequest
+        };
+        console.log("newRequest ", newRequest);
+
+        // Send the POST request.
+        $.ajax("/api/whatever", {
+        type: "POST",
+            data: newRequest
+        }).then(
+            function () {
+        console.log("added new request");
+    // Reload the page to get the updated list
+    location.reload();
+            }
+        );
+    });
+
+$("#respond").on("click", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+    let inputName = $("#name").val().trim();
+    let inputResponse = $("#response").val().trim();
+
+    let newResponse = {
+        user: inputName,
+        post: inputResponse
+    };
+    console.log("newResponse ", newResponse);
+
+
+
+        // Send the request.
+        $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+            data: id
+        }).then(
+            function () {
+        console.log("updated id ", id);
+    // Reload the page to get the updated list
+    location.reload();
+            }
+        );
+    });
+
+
+$(".delete").on("click", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    
+});
