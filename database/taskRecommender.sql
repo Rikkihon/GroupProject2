20bcb637db39e1af181e0625ac8fc6c8c041fe95
Rikki Honnold <rikki@er-10-129-192-216.wireless.umn.edu>
2018-05-26 11:57:42 -0500

    adding the index.js files from sequelize

---
+++ database/taskRecommender.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS TaskRecommender_DB;
+USE TaskRecommender_DB;
+
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS comments;
+
+create table posts (
+    post_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    post varchar(255) NOT NULL,
+    user varchar(255) NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (post_id)
+);
+
+create table comments (
+    id integer(11) AUTO_INCREMENT NOT NULL,
+    user varchar(255) NOT NULL,
+    comment varchar(255),
+    post_id integer,
+    constraint foreign key(post_id) references posts(post_id),
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    primary key (id)
+);
--- models/poster.js
+++ models/poster.js
@@ -1,21 +1,38 @@
-//not sure where to use this or but I think it might be a component that we will need. Unsure.
-module.exports = function(recommender)
-{
-    var recommender = sequelize.define("recommender", {
-        comment: Datatypes.STRING,
-        id: Datatypes.INTEGER
-    })
-    return recommender
-}
-    
-  module.exports = function(requester_needs)  
-  {
-  var request_needs = sequelize.define("request_needs", {
-        post_id: Datatypes.INTEGER,
-        user: Datatypes.STRING,
-        post: Datatypes.STRING,
-     })
-     
-    return request_needs;
+const db = require('../models');
 
-}
+module.exports = function (app) {
+  // GET route
+  app.get('/', (req, res) => {
+    db.Burgers.findAll({}).then((dbBurgers) => {
+      const hbsObject = {
+        burgers: dbBurgers,
+      };
+      res.render('index', hbsObject);
+    });
+  });
+
+  // POST route
+  app.post('/', (req, res) => {
+    db.Burgers.create({
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured,
+    }).then((dbBurgers) => {
+      res.render(dbBurgers);
+    });
+  });
+  // PUT route
+  app.put('/api/burgers/:id', (req, res) => {
+    db.Burgers.update({
+      devoured: true,
+    }, {
+      where: {
+        id: req.params.id,
+      },
+    }).then((dbBurgers) => {
+      res.json('/');
+    });
+  });
+
+
+  // final brace of module export
+};
