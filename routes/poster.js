238c59e0f30905974e1c8cffeb53b9e9fb09af5f
Rikkihon <rikkihon@gmail.com>
2018-06-02 11:10:16 -0500

    Update poster.js
    Commented out some of the CRUD functionality, but it should work when uncommented. - RMH

--- routes/poster.js
+++ routes/poster.js
@@ -1,50 +1,83 @@
-const db = require('../models');
-const express = require("express");
+var db = require('../models');
+var express = require("express");
+const router = require('/Users/rikki/Desktop/GroupProject2/routes/posts.js')
 
-module.exports = function (router) {
+module.exports = function (app) {
   
-//this is the new error
-  router.get('/', (req, res) => {
-    db.request_needs.findAll({}).then((dbrequestneeds) => {
-      const hbsObject = {
-       request_needs: name,
-       request_needs: post
-      };
-      res.json('index', hbsObject);
-      console.log(request_needs);
+
+  app.get('/', (req, res) => {
+      console.log("test")
+      db.request_needs.findAll({
+        include: [db.comments]
+      
+      }).then(function(data){
+        res.send(data);
+        //use res.render when handlebars starts working 
+        console.log(data);
+       {
+         //console log the whole response, 
+         //then the piece from part from sequelize, 
+         //
+         //then get the object built, then log out the data into handlebars, then get the handlebars size 
+      // const hbsObject = {
+      // request_needs: name,
+      // request_needs: post,
+      // };
+      // this is an object that we can render from the html side using res.render on the HTML routes, 
+      //look at handlebars assignment for this. 
+    //res.render('index', hbsObject);// this would render the handlebars object 
+      //console.log(hbsObject);
+    }
     });
   });
 
-  // POST route
-  router.post('/', (req, res) => {
-    db.request_needs.create({
-      request_needs_name: req.body.request_needs_name,
-      devoured: req.body.devoured,
-    }).then((dbrequestneeds) => {
-      res.render(dbrequestneeds);
-    });
-  });
-  // PUT route
-  router.put('/api/request_needs/:id', (req, res) => {
-    db.request_needs.update({
-      devoured: true,
-    }, {
-      where: {
-        id: req.params.id,
-      },
-    }).then((dbrequestneeds) => {
-      res.json('/');
-    });
-  });
-
-// router.delete('/')
-//    db.Comments.destroy({
+//  router.get('/requests', function (req, res) {   
+//   db.request_needs.findOne({
+//   where: {
+//     // id: req.params.id
+//     id: 1,
+//   },
+//   include: [db.comment]
+//   }).then(function(data) {
+//     res.render(db.request_needs);
+//     console.log(data);
+//   });
+//   });
+//   // POST route
+  
+//   router.post('/request_needs', (req, res) => {
+//     db.request_needs.create({
+//       request_needs_name: req.body.request_needs_name,
+//     }).then((dbrequest_needs) => {
+//       res.render(dbrequest_needs);
+//     });
+//   });
+//   // PUT route
+//   let newrequest_needs =  new request_needs('Need new car detailing place, any recommendations?','Jorge');
+  
+//   router.put('/api/request_needs/:id', (req, res) => {
+//     db.request_needs.update(newrequest_needs,{
+//       where: {
+//         //id: req.params.id,
+//         id: 1,
+//       }
+    
+//     }).then((dbrequest_needs) => {
+//       res.json('/');
+//     });
+//   });
+// // 
+// router.delete('/delete_request')
+//    db.request_needs.destroy({
 //        where: {
-//            id: 1
+//           //  id: req.params.id
+//           id: 1
+//            //we will want to make this an input
 //        }
-//    }).then(db.Comments)
+//    }).then(db.request_needs)
 //     {
-//     console.log("This should have deleted things");    
+//     console.log("This should have deleted things");   
+//     console.log(db.request_needsf);    
 //     }
 
   // final brace of module export
