c62181dd2d45e2b13b3b392b1b2f1da0f3c0d318
Rikkihon <rikkihon@gmail.com>
2018-06-02 10:59:25 -0500

    Update seeds.sql

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,7 +1,7 @@
 
-INSERT INTO posts ( post, user) VALUES ( 'Need babysitter for Thursday Night when I am in class', 'Javan' );
-INSERT INTO posts ( post, user) VALUES ('Need Mechanic for broken dishwasher. Under $50, please ', 'Carl' );
-INSERT INTO posts ( post, user) VALUES ('Need Lawn mowed for about 4 weeks while I am in Florida. Will pay 100 for the month', 'Tina');
+INSERT INTO request_needs ( post, user) VALUES ( 'Need babysitter for Thursday Night when I am in class', 'Javan' );
+INSERT INTO request_needs  ( post, user) VALUES ('Need Mechanic for broken dishwasher. Under $50, please ', 'Carl' );
+INSERT INTO request_needs  ( post, user) VALUES ('Need Lawn mowed for about 4 weeks while I am in Florida. Will pay 100 for the month', 'Tina');
 
 INSERT INTO comments (comment, user) VALUES ('I will be home on Thursday if you want to have me watch your kid at my house', 'Pat');
 INSERT INTO comments (comment, user) VALUES ('I know of a teenager who can do this for you while you are gone. I will get back to you.', 'Ed');
